73640ba14f80ba72853ea1cb9d5d009bd450b53e
Rabah Meradi <rabah.meradi@rakuten.com>
2021-12-28 22:07:26 +0100

    Create main program

---
+++ main.ts
@@ -0,0 +1,44 @@
+import {Mower} from "./mower/Mower";
+import {Direction, PositionWithOrientation} from "./mower/position";
+import {Position} from "./mower/Position";
+import {Surface} from "./mower/Surface";
+import eventEmitter from "./mower/events/event-emitter";
+import {MowerController} from "./mower/controller/MowerController";
+
+const mower = new Mower("1", new PositionWithOrientation(new Position(1,2), Direction.N))
+const surface = new Surface(new Position(0,0), new Position(5,5))
+mower.setSurfaceToMow(surface)
+
+let output
+let previousPositionWithOrientation = new PositionWithOrientation(new Position(1,2), Direction.N)
+
+eventEmitter.onRotate((id: string, previousDirection, nextDirection ) => {
+    previousPositionWithOrientation.direction = nextDirection
+})
+
+eventEmitter.onMove((id: string, previousPosition, nextPosition ) => {
+    output = `${nextPosition.x} ${nextPosition.y} ${Direction[previousPositionWithOrientation.direction]}`
+    previousPositionWithOrientation.position = new Position(nextPosition.x, nextPosition.y)
+})
+
+eventEmitter.onEndProcessing(() => {
+    console.log(output)
+})
+eventEmitter.onStartProcessing((id, positionWithOrientation) => {
+    previousPositionWithOrientation = new PositionWithOrientation(
+        new Position(positionWithOrientation.position.x, positionWithOrientation.position.y),
+        positionWithOrientation.direction
+    )
+    output = ''
+})
+
+const main = async () => {
+    const file = process.argv[2]
+    if (!file) {
+        throw new Error("You should provide an instruction file")
+    }
+    const mowerController = new MowerController(process.argv[2])
+    await mowerController.start()
+}
+
+main()
